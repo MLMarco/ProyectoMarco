aa9ad3ea084aec0afee9d0fe67885ff29c460531
⚜️ Marco ⚜️ <morenolechugamarcoantonio@gmail.com>
2023-04-08 22:55:46 -0600

    08/04/2023 blog update

---
+++ script.js
@@ -0,0 +1,134 @@
+document.write("<h3> U s o  D e  J a v a  S c r i p t <br> </h3>");
+document.write("<h4>T a b l a s D e M u l t i p l i c a r </h4>");
+
+let vTabla = 1
+function tablasM() {
+    
+}
+document.write(vTabla + " x 1: " + vTabla * 1 + "<br>");
+document.write(vTabla + " x 2: " + vTabla * 2 + "<br>"); 
+document.write(vTabla + " x 3: " + vTabla * 3 + "<br>"); 
+document.write(vTabla + " x 4: " + vTabla * 4 + "<br>"); 
+document.write(vTabla + " x 5: " + vTabla * 5 + "<br>"); 
+document.write(vTabla + " x 6: " + vTabla * 6 + "<br>"); 
+document.write(vTabla + " x 7: " + vTabla * 7 + "<br>"); 
+document.write(vTabla + " x 8: " + vTabla * 8 + "<br>"); 
+document.write(vTabla + " x 9: " + vTabla * 9 + "<br>"); 
+document.write(vTabla + " x 10: " + vTabla * 10 + "<br>"); 
+
+vTabla = 2
+    document.write("<br>");
+document.write(vTabla + " x 1: " + vTabla * 1 + "<br>");
+document.write(vTabla + " x 2: " + vTabla * 2 + "<br>"); 
+document.write(vTabla + " x 3: " + vTabla * 3 + "<br>"); 
+document.write(vTabla + " x 4: " + vTabla * 4 + "<br>"); 
+document.write(vTabla + " x 5: " + vTabla * 5 + "<br>"); 
+document.write(vTabla + " x 6: " + vTabla * 6 + "<br>"); 
+document.write(vTabla + " x 7: " + vTabla * 7 + "<br>"); 
+document.write(vTabla + " x 8: " + vTabla * 8 + "<br>"); 
+document.write(vTabla + " x 9: " + vTabla * 9 + "<br>"); 
+document.write(vTabla + " x 10: " + vTabla * 10 + "<br>");
+
+vTabla = 3
+    document.write("<br>");
+document.write(vTabla + " x 1: " + vTabla * 1 + "<br>");
+document.write(vTabla + " x 2: " + vTabla * 2 + "<br>"); 
+document.write(vTabla + " x 3: " + vTabla * 3 + "<br>"); 
+document.write(vTabla + " x 4: " + vTabla * 4 + "<br>"); 
+document.write(vTabla + " x 5: " + vTabla * 5 + "<br>"); 
+document.write(vTabla + " x 6: " + vTabla * 6 + "<br>"); 
+document.write(vTabla + " x 7: " + vTabla * 7 + "<br>"); 
+document.write(vTabla + " x 8: " + vTabla * 8 + "<br>"); 
+document.write(vTabla + " x 9: " + vTabla * 9 + "<br>"); 
+document.write(vTabla + " x 10: " + vTabla * 10 + "<br>");
+
+vTabla = 4
+    document.write("<br>");
+document.write(vTabla + " x 1: " + vTabla * 1 + "<br>");
+document.write(vTabla + " x 2: " + vTabla * 2 + "<br>"); 
+document.write(vTabla + " x 3: " + vTabla * 3 + "<br>"); 
+document.write(vTabla + " x 4: " + vTabla * 4 + "<br>"); 
+document.write(vTabla + " x 5: " + vTabla * 5 + "<br>"); 
+document.write(vTabla + " x 6: " + vTabla * 6 + "<br>"); 
+document.write(vTabla + " x 7: " + vTabla * 7 + "<br>"); 
+document.write(vTabla + " x 8: " + vTabla * 8 + "<br>"); 
+document.write(vTabla + " x 9: " + vTabla * 9 + "<br>"); 
+document.write(vTabla + " x 10: " + vTabla * 10 + "<br>");
+
+vTabla = 5
+    document.write("<br>");
+document.write(vTabla + " x 1: " + vTabla * 1 + "<br>");
+document.write(vTabla + " x 2: " + vTabla * 2 + "<br>"); 
+document.write(vTabla + " x 3: " + vTabla * 3 + "<br>"); 
+document.write(vTabla + " x 4: " + vTabla * 4 + "<br>"); 
+document.write(vTabla + " x 5: " + vTabla * 5 + "<br>"); 
+document.write(vTabla + " x 6: " + vTabla * 6 + "<br>"); 
+document.write(vTabla + " x 7: " + vTabla * 7 + "<br>"); 
+document.write(vTabla + " x 8: " + vTabla * 8 + "<br>"); 
+document.write(vTabla + " x 9: " + vTabla * 9 + "<br>"); 
+document.write(vTabla + " x 10: " + vTabla * 10 + "<br>");
+
+vTabla = 6
+    document.write("<br>");
+document.write(vTabla + " x 1: " + vTabla * 1 + "<br>");
+document.write(vTabla + " x 2: " + vTabla * 2 + "<br>"); 
+document.write(vTabla + " x 3: " + vTabla * 3 + "<br>"); 
+document.write(vTabla + " x 4: " + vTabla * 4 + "<br>"); 
+document.write(vTabla + " x 5: " + vTabla * 5 + "<br>"); 
+document.write(vTabla + " x 6: " + vTabla * 6 + "<br>"); 
+document.write(vTabla + " x 7: " + vTabla * 7 + "<br>"); 
+document.write(vTabla + " x 8: " + vTabla * 8 + "<br>"); 
+document.write(vTabla + " x 9: " + vTabla * 9 + "<br>"); 
+document.write(vTabla + " x 10: " + vTabla * 10 + "<br>");
+
+vTabla = 7
+    document.write("<br>");
+document.write(vTabla + " x 1: " + vTabla * 1 + "<br>");
+document.write(vTabla + " x 2: " + vTabla * 2 + "<br>"); 
+document.write(vTabla + " x 3: " + vTabla * 3 + "<br>"); 
+document.write(vTabla + " x 4: " + vTabla * 4 + "<br>"); 
+document.write(vTabla + " x 5: " + vTabla * 5 + "<br>"); 
+document.write(vTabla + " x 6: " + vTabla * 6 + "<br>"); 
+document.write(vTabla + " x 7: " + vTabla * 7 + "<br>"); 
+document.write(vTabla + " x 8: " + vTabla * 8 + "<br>"); 
+document.write(vTabla + " x 9: " + vTabla * 9 + "<br>"); 
+document.write(vTabla + " x 10: " + vTabla * 10 + "<br>");
+
+vTabla = 8
+    document.write("<br>");
+document.write(vTabla + " x 1: " + vTabla * 1 + "<br>");
+document.write(vTabla + " x 2: " + vTabla * 2 + "<br>"); 
+document.write(vTabla + " x 3: " + vTabla * 3 + "<br>"); 
+document.write(vTabla + " x 4: " + vTabla * 4 + "<br>"); 
+document.write(vTabla + " x 5: " + vTabla * 5 + "<br>"); 
+document.write(vTabla + " x 6: " + vTabla * 6 + "<br>"); 
+document.write(vTabla + " x 7: " + vTabla * 7 + "<br>"); 
+document.write(vTabla + " x 8: " + vTabla * 8 + "<br>"); 
+document.write(vTabla + " x 9: " + vTabla * 9 + "<br>"); 
+document.write(vTabla + " x 10: " + vTabla * 10 + "<br>");
+
+vTabla = 9
+    document.write("<br>");
+document.write(vTabla + " x 1: " + vTabla * 1 + "<br>");
+document.write(vTabla + " x 2: " + vTabla * 2 + "<br>"); 
+document.write(vTabla + " x 3: " + vTabla * 3 + "<br>"); 
+document.write(vTabla + " x 4: " + vTabla * 4 + "<br>"); 
+document.write(vTabla + " x 5: " + vTabla * 5 + "<br>"); 
+document.write(vTabla + " x 6: " + vTabla * 6 + "<br>"); 
+document.write(vTabla + " x 7: " + vTabla * 7 + "<br>"); 
+document.write(vTabla + " x 8: " + vTabla * 8 + "<br>"); 
+document.write(vTabla + " x 9: " + vTabla * 9 + "<br>"); 
+document.write(vTabla + " x 10: " + vTabla * 10 + "<br>");
+
+vTabla = 10
+    document.write("<br>");
+document.write(vTabla + " x 1: " + vTabla * 1 + "<br>");
+document.write(vTabla + " x 2: " + vTabla * 2 + "<br>"); 
+document.write(vTabla + " x 3: " + vTabla * 3 + "<br>"); 
+document.write(vTabla + " x 4: " + vTabla * 4 + "<br>"); 
+document.write(vTabla + " x 5: " + vTabla * 5 + "<br>"); 
+document.write(vTabla + " x 6: " + vTabla * 6 + "<br>"); 
+document.write(vTabla + " x 7: " + vTabla * 7 + "<br>"); 
+document.write(vTabla + " x 8: " + vTabla * 8 + "<br>"); 
+document.write(vTabla + " x 9: " + vTabla * 9 + "<br>"); 
+document.write(vTabla + " x 10: " + vTabla * 10 + "<br>");
